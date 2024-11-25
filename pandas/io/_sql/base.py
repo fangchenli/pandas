03d3d9db72cee3ad2c94ab07ff6303e26136c49b
Fangchen Li <fangchen.li@outlook.com>
2024-11-24 23:00:38 -0800

    fix import

---
+++ pandas/io/_sql/base.py
@@ -0,0 +1,166 @@
+from typing import (
+    Any,
+    Literal,
+)
+
+import numpy as np
+
+from pandas._libs import lib
+from pandas._libs.tslibs.nattype import NaTType
+from pandas._typing import (
+    DtypeArg,
+    DtypeBackend,
+)
+from pandas.compat._optional import import_optional_dependency
+
+from pandas.core.api import (
+    DataFrame,
+    DatetimeIndex,
+    Series,
+)
+from pandas.core.arrays import ArrowExtensionArray
+from pandas.core.internals.construction import convert_object_array
+from pandas.core.tools.datetimes import (
+    DatetimeScalar,
+    DatetimeScalarOrArrayConvertible,
+    DatetimeTZDtype,
+    DictConvertible,
+    to_datetime,
+)
+
+
+def _process_parse_dates_argument(
+    parse_dates: (
+        list[str]
+        | dict[str, str]
+        | dict[str, DatetimeScalarOrArrayConvertible | DictConvertible]
+        | None
+    ) = None,
+) -> list[str]:
+    """Process parse_dates argument for read_sql functions"""
+    # handle non-list entries for parse_dates gracefully
+    if parse_dates is True or parse_dates is None or parse_dates is False:
+        parse_dates = []
+
+    elif not hasattr(parse_dates, "__iter__"):
+        parse_dates = [parse_dates]
+    return parse_dates
+
+
+def _handle_date_column(
+    col: DatetimeScalar, utc: bool = False, format: str | dict[str, Any] | None = None
+) -> DatetimeIndex | Series | DatetimeScalar | NaTType | None:
+    if isinstance(format, dict):
+        # GH35185 Allow custom error values in parse_dates argument of
+        # read_sql like functions.
+        # Format can take on custom to_datetime argument values such as
+        # {"errors": "coerce"} or {"dayfirst": True}
+        return to_datetime(col, **format)
+    else:
+        # Allow passing of formatting string for integers
+        # GH17855
+        if format is None and (
+            issubclass(col.dtype.type, np.floating)
+            or issubclass(col.dtype.type, np.integer)
+        ):
+            format = "s"
+        if format in ["D", "d", "h", "m", "s", "ms", "us", "ns"]:
+            return to_datetime(col, errors="coerce", unit=format, utc=utc)
+        elif isinstance(col.dtype, DatetimeTZDtype):
+            # coerce to UTC timezone
+            # GH11216
+            return to_datetime(col, utc=True)
+        else:
+            return to_datetime(col, errors="coerce", format=format, utc=utc)
+
+
+def _parse_date_columns(
+    data_frame: DataFrame,
+    parse_dates: (
+        list[str]
+        | dict[str, str]
+        | dict[str, DatetimeScalarOrArrayConvertible | DictConvertible]
+        | None
+    ) = None,
+) -> DataFrame:
+    """
+    Force non-datetime columns to be read as such.
+    Supports both string formatted and integer timestamp columns.
+    """
+    parse_dates = _process_parse_dates_argument(parse_dates)
+
+    # we want to coerce datetime64_tz dtypes for now to UTC
+    # we could in theory do a 'nice' conversion from a FixedOffset tz
+    # GH11216
+    for i, (col_name, df_col) in enumerate(data_frame.items()):
+        if isinstance(df_col.dtype, DatetimeTZDtype) or col_name in parse_dates:
+            try:
+                fmt = parse_dates[col_name]
+            except (KeyError, TypeError):
+                fmt = None
+            data_frame.isetitem(i, _handle_date_column(df_col, format=fmt))
+
+    return data_frame
+
+
+def _convert_arrays_to_dataframe(
+    data: list,
+    columns: list[str],
+    coerce_float: bool = True,
+    dtype_backend: DtypeBackend | Literal["numpy"] = "numpy",
+) -> DataFrame:
+    content = lib.to_object_array_tuples(data)
+    idx_len = content.shape[0]
+    arrays = convert_object_array(
+        list(content.T),
+        dtype=None,
+        coerce_float=coerce_float,
+        dtype_backend=dtype_backend,
+    )
+    if dtype_backend == "pyarrow":
+        pa = import_optional_dependency("pyarrow")
+
+        result_arrays = []
+        for arr in arrays:
+            pa_array = pa.array(arr, from_pandas=True)
+            if arr.dtype == "string":
+                # TODO: Arrow still infers strings arrays as regular strings instead
+                # of large_string, which is what we preserver everywhere else for
+                # dtype_backend="pyarrow". We may want to reconsider this
+                pa_array = pa_array.cast(pa.string())
+            result_arrays.append(ArrowExtensionArray(pa_array))
+        arrays = result_arrays  # type: ignore[assignment]
+    if arrays:
+        return DataFrame._from_arrays(
+            arrays, columns=columns, index=range(idx_len), verify_integrity=False
+        )
+    else:
+        return DataFrame(columns=columns)
+
+
+def wrap_result(
+    data: list,
+    columns: list[str],
+    index_col: str | list[str] | None = None,
+    coerce_float: bool = True,
+    parse_dates: (
+        list[str]
+        | dict[str, str]
+        | dict[str, DatetimeScalarOrArrayConvertible | DictConvertible]
+        | None
+    ) = None,
+    dtype: DtypeArg | None = None,
+    dtype_backend: DtypeBackend | Literal["numpy"] = "numpy",
+) -> DataFrame:
+    """Wrap result set of a SQLAlchemy query in a DataFrame."""
+    frame = _convert_arrays_to_dataframe(data, columns, coerce_float, dtype_backend)
+
+    if dtype:
+        frame = frame.astype(dtype)
+
+    frame = _parse_date_columns(frame, parse_dates)
+
+    if index_col is not None:
+        frame = frame.set_index(index_col)
+
+    return frame
---
+++ pandas/io/_sql/dbapi2.py
@@ -0,0 +1,134 @@
+from abc import abstractmethod
+from collections.abc import (
+    Generator,
+    Iterator,
+    Sequence,
+)
+from typing import (
+    Literal,
+    Protocol,
+)
+
+from pandas._typing import (
+    DtypeArg,
+    DtypeBackend,
+)
+
+from pandas.core.api import DataFrame
+from pandas.core.tools.datetimes import (
+    DatetimeScalarOrArrayConvertible,
+    DictConvertible,
+)
+
+from pandas.io._sql.base import wrap_result
+
+
+class DBAPI2Cursor(Protocol):
+    description: Sequence[Sequence]
+    arraysize: int = 1
+
+    @property
+    def rowcount(self) -> int: ...
+    def close(self) -> None: ...
+    def execute(self, query: str, parameters: Sequence | None) -> None: ...
+    def executemany(self, query: str, params: Sequence[Sequence] | None) -> None: ...
+    def fetchone(self) -> Sequence | None: ...
+    def fetchmany(self, size: int = arraysize) -> Sequence[Sequence]: ...
+    def fetchall(self) -> Sequence[Sequence]: ...
+    def setinputsizes(self, sizes: Sequence) -> None: ...
+    def setoutputsize(self, size: int, column: int | None) -> None: ...
+    @abstractmethod
+    def nextset(self) -> bool | None: ...
+    @abstractmethod
+    def callproc(self, procname: str, args: Sequence | None) -> None: ...
+
+
+class DBAPI2Connection(Protocol):
+    def close(self) -> None: ...
+    def commit(self) -> None: ...
+    def cursor(self) -> DBAPI2Cursor: ...
+    @abstractmethod
+    def rollback(self) -> None: ...
+
+
+class PandasDBAPI2Interface:
+    def __init__(self, conn: DBAPI2Connection):
+        self.conn = conn
+
+    @staticmethod
+    def _query_iterator(
+        cursor: DBAPI2Cursor,
+        chunksize: int,
+        columns: list[str],
+        index_col: str | list[str] | None = None,
+        coerce_float: bool = True,
+        parse_dates: (
+            list[str]
+            | dict[str, str]
+            | dict[str, DatetimeScalarOrArrayConvertible | DictConvertible]
+            | None
+        ) = None,
+        dtype: DtypeArg | None = None,
+        dtype_backend: DtypeBackend | Literal["numpy"] = "numpy",
+    ) -> Generator[DataFrame]:
+        has_read_data = False
+        while True:
+            data = cursor.fetchmany(chunksize)
+            if type(data) == tuple:
+                data = list(data)
+            if not data:
+                cursor.close()
+                if not has_read_data:
+                    result = DataFrame.from_records(
+                        [], columns=columns, coerce_float=coerce_float
+                    )
+                    if dtype:
+                        result = result.astype(dtype)
+                    yield result
+                break
+
+            has_read_data = True
+            yield wrap_result(
+                data,
+                columns,
+                index_col=index_col,
+                coerce_float=coerce_float,
+                parse_dates=parse_dates,
+                dtype=dtype,
+                dtype_backend=dtype_backend,
+            )
+
+    def read_query(
+        self,
+        query: str,
+        params: Sequence | None = None,
+        index_col: str | list[str] | None = None,
+        coerce_float: bool = True,
+        parse_dates: (
+            list[str]
+            | dict[str, str]
+            | dict[str, DatetimeScalarOrArrayConvertible | DictConvertible]
+            | None
+        ) = None,
+        chunksize: int | None = None,
+        dtype: DtypeArg | None = None,
+        dtype_backend: DtypeBackend | Literal["numpy"] = "numpy",
+    ) -> DataFrame | Iterator[DataFrame]:
+        cursor = self.conn.cursor()
+        cursor.execute(query, params)
+        columns = [x[0] for x in cursor.description]
+        if chunksize is not None:
+            return self._query_iterator(
+                cursor,
+                chunksize,
+                columns,
+                index_col=index_col,
+                coerce_float=coerce_float,
+                parse_dates=parse_dates,
+                dtype=dtype,
+                dtype_backend=dtype_backend,
+            )
+        data = cursor.fetchall()
+        data = list(data)
+        cursor.close()
+        return wrap_result(data, columns)
